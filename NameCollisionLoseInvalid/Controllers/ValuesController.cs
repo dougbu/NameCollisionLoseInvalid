4c6352f7bed988c6b7b91764f8e248a8cc969104
Doug Bunting <dougbu@microsoft.com>
2018-07-13 12:38:25 -0700

    Minor cleanup

--- NameCollisionLoseInvalid/Controllers/ValuesController.cs
+++ NameCollisionLoseInvalid/Controllers/ValuesController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NameCollisionLoseInvalid.Controllers
 {
+    [ApiController]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
